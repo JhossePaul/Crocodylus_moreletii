0ca5d8e7585e40a08a4c7427ec098b3f947f7e22
Jhosse Paul Márquez Ruíz <mcromaceva@hotmail.com>
2015-08-19 23:33:04 -0500

    Avanes en presentacion

--- Analisis/Analisis.R
+++ Analisis/Analisis.R
@@ -4,17 +4,14 @@
 ################################################################################
 ## Análisis de datos por Biol. Jhosse Paul Márquez Ruíz
 # Paqueterias ####
-library(Matrix)
-library(RColorBrewer)
 library(dplyr)
 library(xts)
 library(dygraphs)
 library(reshape2)
 library(rCharts)
-library(nlme)
+library(lme4)
 library(ggplot2)
 
-# Limpieza de datos
 LHC = read.csv("./Datos/Tidy/LHC.csv")
 ID = LHC[, 1:5]
 LHC = t(LHC[, -(1:5)])
@@ -24,35 +21,35 @@ LHC = xts(LHC,
                               by = "months", length.out = 6)
           )
 
-colores = brewer.pal(12, "Set3")
 
-dygraph(data = LHC[, ID$Tina == 1],
-        main = "LHC", xlab = paste("Tina:", 1), group = "LHC") %>%
-        dyAxis("x", rangePad = 20) %>%
-        dyLegend(show = "never") %>%
-        dyOptions(drawPoints = T, pointSize = 2)
+# dygraph(data = LHC[, ID$Tina == 1],
+#         main = "LHC", xlab = paste("Tina:", 1), group = "LHC") %>%
+#         dyAxis("x", rangePad = 20) %>%
+#         dyLegend(show = "never") %>%
+#         dyOptions(drawPoints = T, pointSize = 2)
 
-
-
+muertos = apply(LHC, 2, function(x) length(na.omit(x))) == 0
+# LHC = LHC[, !muertos]
 LHC = melt(as.data.frame(LHC))
 
-LHC$Tina = rep(ID$Tina, each = 6)
-LHC$Resguardo = as.factor(rep(ID$Resguardo, each = 6))
-LHC$Silvestre = as.factor(rep(ID$Silvestre, each = 6))
+LHC$Tina = factor(rep(ID[!muertos, "Tina"], each = 6))
+LHC$Resguardo = factor(rep(ID[!muertos, "Resguardo"], each = 6))
+LHC$Silvestre = factor(rep(ID[!muertos, "Silvestre"], each = 6))
 names(LHC)[1:2] = c("Marca", "LHC")
-LHC$Marca = as.factor(LHC$Marca)
-LHC$Tiempo = rep(1:6, 64)
-LHC$TinaGrande = as.factor(LHC$Tina)
+LHC$Marca = factor(LHC$Marca)
+LHC$Tiempo = rep(1:6, length(unique(LHC$Marca)))
+LHC$TinaGrande = factor(LHC$Tina)
 levels(LHC$TinaGrande) = rep(1:4, each = 3)
 
+# ggplot(LHC, aes(x = factor(Marca), y = LHC)) +
+#         facet_grid(. ~ TinaGrande + Tina, scales = "free") +
+#         geom_boxplot(aes(color = factor(Resguardo))) +
+#         theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+#         labs(title = "Longitud Hocico-Cloca") +
+#         labs(x = "Marca", y = "LHC", color = "Resguardo")
 
-model = lme(fixed = LHC ~ Tiempo + Resguardo, random = ~ 1| TinaGrande/Tina/Marca,
-            data  = LHC, na.action = na.exclude, method = "ML")
+model = lmer(LHC ~ Resguardo + (1|TinaGrande/Tina/Marca), data = LHC)
+anova(model)
 summary(model)
 
-ggplot(LHC, aes(x = factor(Marca), y = LHC)) +
-        facet_grid(. ~ TinaGrande + Tina, scales = "free") +
-        geom_boxplot(aes(color = factor(Resguardo))) +
-        theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-        labs(title = "Longitud Hocico-Cloca") +
-        labs(x = "Marca", y = "LHC", color = "Resguardo")
+
